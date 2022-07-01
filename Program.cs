4cfdf75d9a71a7c7ef964aaa59e00c1c1524dd4d
Xcal1burBG <99122623+Xcal1burBG@users.noreply.github.com>
2022-07-01 10:16:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string timeConversion(string s)
+    {
+        string time = s.Substring(0, 8);
+        string partOfTheDay = s.Substring(8, 2);
+        string hoursAsString = s.Substring(0, 2);
+        string minutesAsString = s.Substring(3, 2);
+        string secondsAsString = s.Substring(6, 2);
+
+        int[] timeAsArray = time.Split(":").Select(x => int.Parse(x)).ToArray();
+        int hours = timeAsArray[0];
+        int minutes = timeAsArray[1];
+        int seconds = timeAsArray[2];
+
+        if (hoursAsString == "12" && partOfTheDay == "AM")
+        {
+            hours -= 12;
+        }
+
+        if (hours >= 1 && hours <= 11 && partOfTheDay == "PM")
+        {
+            hours += 12;
+        }
+
+        if (hours >= 0 && hours <= 9)
+        {
+            hoursAsString = $"0{hours}";
+        }
+
+        if (hours > 9)
+        {
+            hoursAsString = hours.ToString();
+        }
+
+
+        if (minutes >= 0 && minutes <= 9)
+        {
+            minutesAsString = $"0{minutes}";
+        }
+
+        if (seconds >= 0 && seconds <= 9)
+        {
+            secondsAsString = $"0{seconds}";
+        }
+
+
+        return $"{hoursAsString}:{minutesAsString}:{secondsAsString}";
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string s = Console.ReadLine();
+
+        string result = Result.timeConversion(s);
+
+        Console.WriteLine(result);
+
+
+    }
+}
